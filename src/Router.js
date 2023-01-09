500f3337b91e53a90ee270d4fc10c75391861eae
Şuayip Işık <suayip.isik.16@gmail.com>
2023-01-09 19:36:39 +0300

    Welcome page added

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Welcome from './pages/Welcome';
 
 const Stack = createNativeStackNavigator();
 
 export default function Router() {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Welcome' component={Welcome} options={{ headerShown: false }} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
---
+++ src/pages/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native';
+import styles from './Welcome.style';
+
+const Welcome = () => {
+    return (
+        <SafeAreaView>
+            <Text>Welcome</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Welcome;
