d4d424303e982bb3a504bf70bf8d23e5e9e3969c
Şuayip Işık <suayip.isik.16@gmail.com>
2023-01-09 20:03:09 +0300

    Welcome page edited

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native';
+import { Image, SafeAreaView, Text, View } from 'react-native';
 import styles from './Welcome.style';
+import DigitusLogo from '../../assets/DigitusLogos/digitus.png';
 
 const Welcome = () => {
     return (
-        <SafeAreaView>
-            <Text>Welcome</Text>
+        <SafeAreaView style={styles.page}>
+            <View style={styles.logoHeader}>
+                <Image style={{ width: '50%', height: '30%' }} source={DigitusLogo} />
+            </View>
         </SafeAreaView>
     )
 }
--- src/pages/Welcome/Welcome.style.js
+++ src/pages/Welcome/Welcome.style.js
@@ -2,5 +2,12 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     page: {
+        flex: 1,
+        backgroundColor: '#ffffff',
     },
+    logoHeader: {
+        width: '100%',
+        paddingVertical: 30,
+        alignItems: 'center'
+    }
 });
