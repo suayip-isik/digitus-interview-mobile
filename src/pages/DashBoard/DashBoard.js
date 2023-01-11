c8d6ad19fa488ad5b1ba5f3bf9d5453afb5e0bf8
Şuayip Işık <suayip.isik.16@gmail.com>
2023-01-11 11:49:54 +0300

    edit

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -21,7 +21,6 @@ const DashBoard = () => {
         <SafeAreaView style={styles.page}>
 
             <View style={styles.story}>
-
                 <ScrollView style={{ flex: 1 }} horizontal showsHorizontalScrollIndicator={false}>
                     {
                         images.map((val, i) => {
